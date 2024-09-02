6a0b67cafd569906c1fc99ebca38d92679394877
Gabriel okiri <gabriel@nubytes.us>
2024-09-02 14:10:30 +0100

    terraform infra

--- gke.tf
+++ gke.tf
@@ -18,7 +18,7 @@ resource "google_container_cluster" "primary" {
   # node pool and immediately delete it.
   remove_default_node_pool = true
   initial_node_count       = 1
-deletion_protection = true
+deletion_protection = false
   node_config {
     disk_size_gb = 50
   }
