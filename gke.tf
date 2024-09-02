d1cf3be652095aed3d1f13443713d1e4bf542121
Gabriel okiri <gabriel@nubytes.us>
2024-09-02 13:18:20 +0100

    terraform infra

--- gke.tf
+++ gke.tf
@@ -1,34 +1,50 @@
-resource "google_container_cluster" "primary" {
-  name     = "gke-cluster"
-  location = var.region
+# resource "google_container_cluster" "primary" {
+#   name     = "gke-cluster"
+#   location = var.region
 
+#   remove_default_node_pool = true
+#   initial_node_count       = 1
+
+#   network    = google_compute_network.vpc.name
+#   subnetwork = google_compute_subnetwork.subnet.name
+# }
+
+resource "google_container_cluster" "primary" {
+  name     = "k8s-cluster"
+ location = var.region
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
   remove_default_node_pool = true
   initial_node_count       = 1
 
-  network    = google_compute_network.vpc.name
-  subnetwork = google_compute_subnetwork.subnet.name
-}
-
-resource "google_container_node_pool" "primary_nodes" {
-  name       = "node-pool"
-  location   = var.region
-  cluster    = google_container_cluster.primary.name
-  node_count = var.gke_num_nodes
-
   node_config {
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
-
-    labels = {
-      env = var.project_id
-    }
-   disk_size_gb = 10 # 
-    machine_type = var.machine_type
-    tags         = ["gke-node", "${var.project_id}-gke"]
-    metadata = {
-      disable-legacy-endpoints = "true"
-    }
+    disk_size_gb = 50
   }
+
 }
+
+# resource "google_container_node_pool" "primary_nodes" {
+#   name       = "node-pool"
+#   location   = var.region
+#   cluster    = google_container_cluster.primary.name
+#   node_count = var.gke_num_nodes
+
+#   node_config {
+#     oauth_scopes = [
+#       "https://www.googleapis.com/auth/logging.write",
+#       "https://www.googleapis.com/auth/monitoring",
+#     ]
+
+#     labels = {
+#       env = var.project_id
+#     }
+#    disk_size_gb = 10 # 
+#     machine_type = var.machine_type
+#     tags         = ["gke-node", "${var.project_id}-gke"]
+#     metadata = {
+#       disable-legacy-endpoints = "true"
+#     }
+#   }
+# }
