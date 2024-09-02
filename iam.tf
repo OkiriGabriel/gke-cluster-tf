0498dba442b46760d1fc44cdc31dc265938c2a93
Gabriel okiri <gabriel@nubytes.us>
2024-09-02 16:54:18 +0100

    terraform infra

--- iam.tf
+++ iam.tf
@@ -1,26 +1,26 @@
-resource "google_service_account" "gke_sa" {
-  account_id   = "shortlet-api"
-  display_name = "gke-k8s"
-}
+# resource "google_service_account" "gke_sa" {
+#   account_id   = "shortlet-api"
+#   display_name = "gke-k8s"
+# }
 
-resource "google_project_iam_member" "gke_sa_roles" {
-  for_each = toset([
-    "roles/container.admin",
-    "roles/storage.objectViewer",
-    "roles/logging.logWriter",
-    "roles/monitoring.metricWriter",
-  ])
+# resource "google_project_iam_member" "gke_sa_roles" {
+#   for_each = toset([
+#     "roles/container.admin",
+#     "roles/storage.objectViewer",
+#     "roles/logging.logWriter",
+#     "roles/monitoring.metricWriter",
+#   ])
   
-  project = var.project_id
-  role    = each.key
-  member  = "serviceAccount:${google_service_account.gke_sa.email}"
-}
+#   project = var.project_id
+#   role    = each.key
+#   member  = "serviceAccount:${google_service_account.gke_sa.email}"
+# }
 
-resource "google_service_account_iam_binding" "gke_sa_user" {
-  service_account_id = google_service_account.gke_sa.name
-  role               = "roles/iam.serviceAccountUser"
+# resource "google_service_account_iam_binding" "gke_sa_user" {
+#   service_account_id = google_service_account.gke_sa.name
+#   role               = "roles/iam.serviceAccountUser"
 
-  members = [
-    "serviceAccount:${google_service_account.gke_sa.email}",
-  ]
-}
+#   members = [
+#     "serviceAccount:${google_service_account.gke_sa.email}",
+#   ]
+# }
